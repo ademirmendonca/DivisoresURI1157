dc73221ac5387a37e449fd0840fbc5efaf0e54d0
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-26 17:05:37 -0300

    .

---
+++ DivisoresURI1157/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DivisoresURI1157
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= n; i++)
+            {
+                int total = n % i;
+                if (total == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            
+        }
+    }
+}
